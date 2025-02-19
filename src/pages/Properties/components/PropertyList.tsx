206a13a3ead10d32431773e6bc6f457f97a60adc
Arpit Rathi <arpit97rathi@gmail.com>
2025-02-19 23:02:04 +0530

    feat: integrate properties list api

--- src/pages/Properties/components/PropertyList.tsx
+++ src/pages/Properties/components/PropertyList.tsx
@@ -10,7 +10,7 @@ const PropertyList = ({ data, searchString }: Props) => {
     <>
       {
         data.map((d: any) => {
-          if (!searchString || d?.name?.includes(searchString))
+          if (!searchString || d?.name?.toUpperCase()?.includes(searchString.toUpperCase()) || d?.address?.toUpperCase()?.includes(searchString.toUpperCase()))
             return <PropertyCard key={d?.propertyId} data={d} />   
         })
       }
